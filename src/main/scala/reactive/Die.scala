9b2331e1f14b87f5c99eecf18c7b0a87a49ecdcc
Pim Verkerk <pimverkerk@hotmail.com>
2016-03-07 23:57:43 +0100

    Removed solition

--- src/main/scala/reactive/Die.scala
+++ src/main/scala/reactive/Die.scala
@@ -1,11 +1,6 @@
 package reactive
 
-import spray.json.DefaultJsonProtocol
+//import spray.json.DefaultJsonProtocol
 
 case class Die(sides: Int)
 case class Roll(die: Die, result: Int)
-
-object RollProtocol extends DefaultJsonProtocol {
-  implicit val fmtDie = jsonFormat1(Die.apply)
-	implicit val fmtRoll = jsonFormat2(Roll.apply)
-}
--- src/main/scala/reactive/Main.scala
+++ src/main/scala/reactive/Main.scala
@@ -12,12 +12,6 @@ import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.javadsl.server.values.Headers
 import akka.http.scaladsl.model.ContentType
 import akka.http.scaladsl.model.MediaType
-import scala.concurrent.Future
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import RollProtocol._
-import akka.pattern.CircuitBreaker
-import scala.util.Success
 
 object Main extends App {
   implicit val system = ActorSystem("die-api")
@@ -27,24 +21,14 @@ object Main extends App {
   implicit val materializer = ActorMaterializer()
   val serverBinding = Http().bindAndHandle(interface = "0.0.0.0", port = 8080, handler = mainFlow)
 
-  val guard = CircuitBreaker.apply(system.scheduler, 1, 1.second, 5.second)
+  //val guard = CircuitBreaker(system.scheduler, 1, 1.second, 5.second)
 
   def mainFlow(implicit system: ActorSystem, timeout: Timeout, executor: ExecutionContext): Route = {
     get {
-      onSuccess(Roller.roll(Die(6))) { roll =>
-        complete {
-          roll
-        }
-      }
+      ???
     }
     path("slow") {
-      get {
-        val guarded = guard.withCircuitBreaker(Roller.rollSlow(Die(6)))
-        onComplete(guarded) {
-          case Success(roll) => complete(roll)
-          case _             => complete("Server Busy")
-        }
-      }
+      ???
     }
   }
 }
